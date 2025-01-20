728e4c24bcf397ea7782ae267f700cf450cf062f
Anton Korobeynikov <anton@korobeynikov.info>
2025-01-19 21:47:47 -0800

    Add translator stub, P4C side
    Signed-off-by: Anton Korobeynikov <anton@korobeynikov.info>

---
+++ tools/p4mlir-translate/main.cpp
@@ -0,0 +1,125 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <iostream>
+
+#include "frontends/common/constantFolding.h"
+#include "frontends/common/parseInput.h"
+#include "frontends/common/parser_options.h"
+#include "frontends/p4/checkNamedArgs.h"
+#include "frontends/p4/createBuiltins.h"
+#include "frontends/p4/frontend.h"
+#include "frontends/p4/toP4/toP4.h"
+#include "frontends/p4/validateParsedProgram.h"
+#include "frontends/p4/validateStringAnnotations.h"
+#include "ir/ir.h"
+#include "ir/visitor.h"
+#include "lib/compile_context.h"
+#include "lib/crash.h"
+#include "lib/error.h"
+#include "lib/gc.h"
+#include "options.h"
+
+namespace {
+void log_dump(const P4::IR::Node *node, const char *head) {
+    if (node && LOGGING(1)) {
+        if (head)
+            std::cout << '+' << std::setw(strlen(head) + 6) << std::setfill('-') << "+\n| " << head
+                      << " |\n"
+                      << '+' << std::setw(strlen(head) + 3) << "+" << std::endl
+                      << std::setfill(' ');
+        if (LOGGING(2))
+            dump(node);
+        else
+            std::cout << *node << std::endl;
+    }
+}
+
+/** Changes the value of strictStruct in the typeMap */
+class SetStrictStruct : public P4::Inspector {
+    P4::TypeMap *typeMap;
+    bool strictStruct;
+
+ public:
+    SetStrictStruct(P4::TypeMap *typeMap, bool strict) : typeMap(typeMap), strictStruct(strict) {}
+    bool preorder(const P4::IR::P4Program *) override { return false; }
+    Visitor::profile_t init_apply(const P4::IR::Node *node) override {
+        typeMap->setStrictStruct(strictStruct);
+        return Inspector::init_apply(node);
+    }
+};
+
+}  // namespace
+
+int main(int argc, char *const argv[]) {
+    setup_gc_logging();
+    P4::setup_signals();
+
+    P4::AutoCompileContext autoP4MLIRTranslateContext(new P4::MLIR::TranslateContext);
+    auto &options = P4::MLIR::TranslateContext::get().options();
+    options.langVersion = P4::CompilerOptions::FrontendVersion::P4_16;
+
+    if (options.process(argc, argv) == nullptr || P4::errorCount() > 0) return EXIT_FAILURE;
+
+    options.setInputFile();
+    const auto *program = P4::parseP4File(options);
+
+    if (program == nullptr || P4::errorCount() > 0) return EXIT_FAILURE;
+
+    log_dump(program, "Parsed program");
+    auto hook = options.getDebugHook();
+    if (!options.parseOnly) {
+        if (options.typeinferenceOnly) {
+            P4::FrontEndPolicy policy;
+            P4::TypeMap typeMap;
+
+            P4::ParseAnnotations *parseAnnotations = policy.getParseAnnotations();
+            if (!parseAnnotations) parseAnnotations = new P4::ParseAnnotations();
+
+            P4::PassManager passes({
+                // Parse annotations
+                new P4::ParseAnnotationBodies(parseAnnotations, &typeMap),
+                // Simple checks on parsed program
+                new P4::ValidateParsedProgram(),
+                // Synthesize some built-in constructs
+                new P4::CreateBuiltins(),
+                // First pass of constant folding, before types are known --
+                // may be needed to compute types.
+                new P4::ConstantFolding(policy.getConstantFoldingPolicy()),
+                // Validate @name/@deprecated/@noWarn. Should run after constant folding.
+                new P4::ValidateStringAnnotations(),
+                new P4::CheckNamedArgs(),
+                // Type checking and type inference.  Also inserts
+                // explicit casts where implicit casts exist.
+                new SetStrictStruct(&typeMap, true),  // Next pass uses strict struct checking
+                new P4::TypeInference(&typeMap, false, false),  // insert casts, don't check arrays
+                new SetStrictStruct(&typeMap, false),
+            });
+            passes.setName("TypeInference");
+            passes.setStopOnError(true);
+            passes.addDebugHook(hook, true);
+            program = program->apply(passes);
+        } else {
+            // Apply the front end passes. These are usually fixed.
+            P4::FrontEnd fe;
+            fe.addDebugHook(hook);
+            program = fe.run(options, program);
+        }
+    }
+
+    log_dump(program, "After frontend");
+
+    if (P4::Log::verbose()) std::cerr << "Done." << std::endl;
+    return P4::errorCount() > 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+}
---
+++ tools/p4mlir-translate/options.cpp
@@ -0,0 +1,34 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "options.h"
+
+using namespace P4::MLIR;
+
+TranslateOptions::TranslateOptions() {
+    registerOption(
+        "--parse-only", nullptr,
+        [this](const char *) {
+            parseOnly = true;
+            return true;
+        },
+        "only parse the P4 input, without any further processing");
+    registerOption(
+        "--typeinference-only", nullptr,
+        [this](const char *) {
+            typeinferenceOnly = true;
+            return true;
+        },
+        "parse the P4 input and run minimal set of frontend passes for type inference");
+}
