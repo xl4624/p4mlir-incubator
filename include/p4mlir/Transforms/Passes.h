7b4ca3b42d55f160bdbeae8de3494b1350d7dcfe
Anton Korobeynikov <anton@korobeynikov.info>
2025-05-01 16:32:15 +0300

    Add boilerplate for custom passes
    Signed-off-by: Anton Korobeynikov <anton@korobeynikov.info>

---
+++ include/p4mlir/Transforms/Passes.h
@@ -0,0 +1,34 @@
+#ifndef P4MLIR_TRANSFORMS_PASSES_H
+#define P4MLIR_TRANSFORMS_PASSES_H
+
+// We explicitly do not use push / pop for diagnostic in
+// order to propagate pragma further on
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+#include <memory>
+
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Pass/Pass.h"
+#include "p4mlir/Dialect/P4HIR/P4HIR_Attrs.h"
+#include "p4mlir/Dialect/P4HIR/P4HIR_Dialect.h"
+#include "p4mlir/Dialect/P4HIR/P4HIR_Ops.h"
+#include "p4mlir/Dialect/P4HIR/P4HIR_OpsEnums.h"
+#include "p4mlir/Dialect/P4HIR/P4HIR_Types.h"
+
+namespace P4::P4MLIR {
+
+//===----------------------------------------------------------------------===//
+// Passes
+//===----------------------------------------------------------------------===//
+
+#define GEN_PASS_DECL_SIMPLIFYPARSERS
+#include "p4mlir/Transforms/Passes.h.inc"
+
+#define GEN_PASS_REGISTRATION
+#include "p4mlir/Transforms/Passes.h.inc"
+
+}  // namespace P4::P4MLIR
+
+#endif  // P4MLIR_TRANSFORMS_PASSES_H
--- tools/p4mlir-opt/p4mlir-opt.cpp
+++ tools/p4mlir-opt/p4mlir-opt.cpp
@@ -3,16 +3,16 @@
 #include "mlir/InitAllPasses.h"
 #include "mlir/Support/FileUtilities.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
-
 #include "p4mlir/Dialect/P4HIR/P4HIR_Dialect.h"
+#include "p4mlir/Transforms/Passes.h"
 
 int main(int argc, char **argv) {
-  mlir::registerAllPasses();
+    mlir::registerAllPasses();
+    P4::P4MLIR::registerPasses();
 
-  mlir::DialectRegistry registry;
-  registry.insert<P4::P4MLIR::P4HIR::P4HIRDialect,
-                  mlir::func::FuncDialect>();
+    mlir::DialectRegistry registry;
+    registry.insert<P4::P4MLIR::P4HIR::P4HIRDialect, mlir::func::FuncDialect>();
 
-  return mlir::asMainReturnCode(
-      mlir::MlirOptMain(argc, argv, "P4MLIR optimizer driver\n", registry));
+    return mlir::asMainReturnCode(
+        mlir::MlirOptMain(argc, argv, "P4MLIR optimizer driver\n", registry));
 }
